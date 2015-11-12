4a1db2b796a8889914fd24f354a11e745149c77a
gda10jth <gda10jth@exjobb18.cs.lth.se>
2015-11-12 15:22:29 +0100

    fixed bug with expand

--- src/main/java/uicomponent/graph/PopupGraphMousePlugin.java
+++ src/main/java/uicomponent/graph/PopupGraphMousePlugin.java
@@ -128,10 +128,11 @@ class PopupGraphMousePlugin<V, E> extends AbstractPopupGraphMousePlugin{
             GenericTreeNode parent = null;
             UIEdge edge = null;
             if(lastClicked.getParent() != null) {
-                parent = inGraph.getPredecessors(lastClicked).iterator().next();
+                parent = node.getParent();
                 for(UIEdge e : inGraph.getInEdges(lastClicked)) {
-                    if (!e.isReference())
+                    if (!e.isReference()) {
                         edge = e;
+                    }
                 }
                 //edge = inGraph.getInEdges(lastClicked).iterator().next();
             }
@@ -184,7 +185,7 @@ class PopupGraphMousePlugin<V, E> extends AbstractPopupGraphMousePlugin{
             GenericTreeNode parent = null;
             UIEdge edge = null;
             if(lastClicked.getParent() != null) {
-                parent = inGraph.getPredecessors(lastClicked).iterator().next();
+                parent = lastClicked.getParent();
                 for(UIEdge e : inGraph.getInEdges(lastClicked)) {
                     if (!e.isReference())
                         edge = e;
