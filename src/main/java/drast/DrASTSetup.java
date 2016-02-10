ed6f259adfe2a1c51622df6690768bb4733475d2
Joel Lindholm <gda10jli@exjobb25.cs.lth.se>
2016-02-10 17:10:39 +0100

    merge

--- src/main/java/drast/DrASTSetup.java
+++ src/main/java/drast/DrASTSetup.java
@@ -126,24 +126,3 @@ public class DrASTSetup {
     }
 }
 
-class SystemExitControl {
-
-    public static class ExitTrappedException extends SecurityException {
-    }
-
-    public static void forbidSystemExitCall() {
-        final SecurityManager securityManager = new SecurityManager() {
-            @Override
-            public void checkPermission(Permission permission) {
-                if (permission.getName().contains("exitVM")) {
-                    throw new ExitTrappedException();
-                }
-            }
-        };
-        System.setSecurityManager(securityManager);
-    }
-
-    public static void enableSystemExitCall() {
-        System.setSecurityManager(null);
-    }
-}
