b1d044a449b4877996ced92a94afebb5963d3ee8
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2016-11-11 17:36:34 +0100

    Set version name to 1.0.6

--- src/main/java/drast/model/DrAST.java
+++ src/main/java/drast/model/DrAST.java
@@ -10,7 +10,7 @@ import drast.model.filteredtree.GenericTreeNode;
  *
  */
 public class DrAST {
-	public static final String VERSION = "build-1.0.5";
+	public static final String VERSION = "1.0.6";
 
 	public final static String FILE_NAME = "DrAST-result";
     public final static String CLUSTER_STRING = "cluster";
